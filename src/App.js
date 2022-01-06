7c1c0ffa5d5cf6bb0991abfdff612d5ade561479
Samsuddin-Shohan <shohan.cse.hstu@gmail.com>
2022-01-06 19:04:03 +0600

    router added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Route,Switch} from 'react-router-dom';
+import Home from './Components/Home/Home';
+import About from './Components/About/About';
+import Services from './Components/Services/Services';
+import Navbar from './Components/Navbar/Navbar';
+import NotFound from './Components/NotFound/NotFound';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+      <Navbar></Navbar>
+      <Switch>
+      
+
+          
+          <Route exact path='/' component = {Home}></Route>
+          <Route path = '/about' component={About}></Route>
+          <Route path = '/services' component={Services}></Route>
+          <Route component={NotFound}></Route>
+
+      </Switch>
+      </Router>
     </div>
   );
 }
